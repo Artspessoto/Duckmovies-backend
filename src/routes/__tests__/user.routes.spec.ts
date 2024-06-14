6acdba0dd7918f63367fd48774d8e41695c6a445
ryangst <ryangst.hire@gmail.com>
2024-06-14 18:08:26 -0300

    create user creation test

--- src/routes/__tests__/user.routes.spec.ts
+++ src/routes/__tests__/user.routes.spec.ts
@@ -1,8 +1,23 @@
-import {describe, it} from "vitest";
+import {describe, it, expect} from "vitest";
+import supertest from "supertest"
 
+import {App} from "../../server.js"
+
+const hash = (): string => {
+    return Math.random().toString(36).substring(7);
+}
 
 describe("User routes", () => {
-    it("should return a list of users", () => {
+    it("should create an user", async () => {
+        const user = {
+            name: "Garibaldo",
+            email: `vilasesamo-${hash()}@gmail.com`,
+            password: "garibaldinho123"
+        }
 
+        const url = "/users"
+        const response = await supertest(App).post(url).send(user)
+        console.log(response.body)
+        expect(response.status).toEqual(201);
     });
 })
--- src/server.js
+++ src/server.js
@@ -30,4 +30,4 @@ const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
 
-export default app;
+export const App = app;
