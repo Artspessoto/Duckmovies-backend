6c95cbba2d92da744fb4dac06bc97f59d92dbb26
artspessoto <artspessoto@gmail.com>
2024-06-18 15:06:21 -0300

    create user update test, add test for invalid and empty token

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,81 +1,84 @@
-const {hash, compare} = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
 const z = require("zod");
 
 const CreateUserPayload = z.object({
-    email: z.string().email("E-mail inválido"),
-    name: z.string().min(1, "Nome muito curto"),
-    password: z.string().min(6, "Senha muito curta"),
+  email: z.string().email("E-mail inválido"),
+  name: z.string().min(1, "Nome muito curto"),
+  password: z.string().min(6, "Senha muito curta"),
 });
 
 class UserController {
-    async create(req, res) {
-        const {name, email, password} = req.body;
+  async create(req, res) {
+    const { name, email, password } = req.body;
 
-        const {success, error} = CreateUserPayload.safeParse({name, email, password});
+    const { success, error } = CreateUserPayload.safeParse({
+      name,
+      email,
+      password,
+    });
 
+    if (!success)
+      throw new AppError(error.errors.map((err) => err.message).join("\n"));
 
-        if (!success) throw new AppError(error.errors.map((err) => err.message).join("\n"));
+    const checkUserExists = await knex("users").where({ email }).first();
 
+    if (checkUserExists) throw new AppError("Este e-mail já está em uso.");
 
-        const checkUserExists = await knex("users").where({email}).first();
+    const hashedPassword = await hash(password, 8);
 
-        if (checkUserExists) throw new AppError("Este e-mail já está em uso.");
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-        const hashedPassword = await hash(password, 8);
+    return res.status(201).json();
+  }
 
-        await knex("users").insert({
-            name,
-            email,
-            password: hashedPassword,
-        });
+  async update(req, res) {
+    const { name, email, password, old_password } = req.body;
 
-        return res.status(201).json();
+    const user_id = req.user.id;
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user) throw new AppError("Usuário não encontrado", 401);
+
+    const userWithUpdateEmail = await knex("users").where({ email }).first();
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
-    async update(req, res) {
-        const {name, email, password, old_password} = req.body;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
-        const user_id = req.user.id;
-        const user = await knex("users").where({id: user_id}).first();
-
-        if (!user) throw new AppError("Usuário não encontrado", 401);
-
-        const userWithUpdateEmail = await knex("users").where({email}).first();
-
-        if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
-            throw new AppError("Este e-mail já está em uso.");
-        }
-
-        user.name = name ?? user.name;
-        user.email = email ?? user.email;
-
-        if (password && !old_password) {
-            throw new AppError(
-                "Você precisa informar a senha antiga para definir a nova senha"
-            );
-        }
-
-        if (password && old_password) {
-            const checkOldPassword = await compare(old_password, user.password);
-
-            if (!checkOldPassword) throw new AppError("A senha antiga não confere");
-
-            user.password = await hash(password, 8);
-        }
-
-        await knex("users")
-            .update({
-                name: user.name,
-                email: user.email,
-                password: user.password,
-                updated_at: knex.raw("CURRENT_TIMESTAMP"),
-            })
-            .where({id: user_id});
-
-        return res.status(200).json();
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha"
+      );
     }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) throw new AppError("A senha antiga não confere");
+
+      user.password = await hash(password, 8);
+    }
+
+    await knex("users")
+      .update({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        updated_at: knex.raw("CURRENT_TIMESTAMP"),
+      })
+      .where({ id: user_id });
+
+    return res.status(200).json();
+  }
 }
 
 module.exports = UserController;
--- src/routes/__tests__/user.routes.spec.ts
+++ src/routes/__tests__/user.routes.spec.ts
@@ -1,53 +1,94 @@
-import {describe, it, expect} from "vitest";
-import supertest from "supertest"
+import { describe, it, expect } from "vitest";
+import supertest from "supertest";
 
-import {App} from "../../server.js"
+import { App } from "../../server.js";
 
 const hash = (): string => {
-    return Math.random().toString(36).substring(7);
-}
+  return Math.random().toString(36).substring(7);
+};
 
 describe("User routes", () => {
-    it("should create an user", async () => {
-        const user = {
-            name: "Garibaldo",
-            email: `vilasesamo-${hash()}@gmail.com`,
-            password: "garibaldinho123"
-        }
+  it("should create an user", async () => {
+    const user = {
+      name: "Garibaldo",
+      email: `vilasesamo-${hash()}@gmail.com`,
+      password: "garibaldinho123",
+    };
 
-        const url = "/users"
-        const response = await supertest(App).post(url).send(user)
+    const url = "/users";
+    const response = await supertest(App).post(url).send(user);
 
-        expect(response.status).toEqual(201);
+    expect(response.status).toEqual(201);
+  });
+
+  it("should not create an user with an empty email", async () => {
+    const user = {
+      name: "Garibaldo",
+      email: "",
+      password: "garibaldinho123",
+    };
+
+    const url = "/users";
+    const response = await supertest(App).post(url).send(user);
+    expect(response.status).toEqual(400);
+  });
+
+  it("should not create an user with an empty password", async () => {
+    const user = {
+      name: "Garibaldo",
+      email: `vilasesamo-${hash()}@gmail.com`,
+      password: "",
+    };
+
+    const url = "/users";
+    const response = await supertest(App).post(url).send(user);
+
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual({
+      status: "Error",
+      message: "Senha muito curta",
     });
+  });
 
-    it.skip("should not create an user with an empty email", async () => {
-        const user = {
-            name: "Garibaldo",
-            email: "",
-            password: "garibaldinho123"
-        }
+  it("should not update an user with an empty token", async () => {
+    const user = {
+      name: "Garibaldo",
+      email: "vilasesamo-663zls@gmail.com",
+      password: "garibaldinho123",
+      token: "",
+    };
 
-        const url = "/users"
-        const response = await supertest(App).post(url).send(user)
-        expect(response.status).toEqual(201);
+    const url = "/users";
+    const response = await supertest(App)
+      .put(url)
+      .set("Authorization", `Bearer ${user.token}`)
+      .send(user);
+
+    expect(response.status).toEqual(401);
+    expect(response.body).toEqual({
+      status: "Error",
+      message: "JWT Token inválido",
     });
+  });
 
+  it("should not update and user with an invalid token", async () => {
+    const user = {
+      name: "Garibaldo",
+      email: "vilasesamo-663zls@gmail.com",
+      password: "garibaldinho123",
+      token: "invalid-token",
+    };
 
-    it("should not create an user with an empty password", async () => {
-        const user = {
-            name: "Garibaldo",
-            email: `vilasesamo-${hash()}@gmail.com`,
-            password: ""
-        }
+    const url = "/users";
+    const response = await supertest(App)
+      .put(url)
+      .set("Authorization", `Bearer ${user.token}`)
+      .send(user);
 
-        const url = "/users"
-        const response = await supertest(App).post(url).send(user)
-
-        expect(response.status).toEqual(400);
-        expect(response.body).toEqual({
-            status: "Error",
-            message: "Senha muito curta"
-        })
+    expect(response.status).toEqual(401);
+    expect(response.body).toEqual({
+      status: "Error",
+      message: "JWT Token inválido",
     });
-})
+  });
+});
