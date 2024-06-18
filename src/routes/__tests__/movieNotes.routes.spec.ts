e8f15d93d33225c3e4f818d7aeb12a4f169ca157
artspessoto <artspessoto@gmail.com>
2024-06-18 19:25:34 -0300

    create movie_notes creation test

--- src/routes/__tests__/movieNotes.routes.spec.ts
+++ src/routes/__tests__/movieNotes.routes.spec.ts
@@ -4,5 +4,34 @@ import supertest from "supertest";
 import { App } from "../../server.js";
 
 describe("MovieNotes routes", () => {
-    
-})
+  it("should create an movie_note", async () => {
+    const userData = {
+      email: "vilasesamo-663zls@gmail.com",
+      password: "garibaldinho123",
+    };
+
+    const movieNote = {
+      title: "Pânico",
+      description: `Um grupo de jovens enfrenta um assassino mascarado 
+            que testa seus conhecimentos sobre filmes de terror. 
+            A pequena cidade de Woodsboro nunca mais será a mesma.`,
+      rating: 4,
+      tags: ["Terror", "Mistério"],
+    };
+
+    const urlToken = "/sessions";
+    const authResponse = await supertest(App)
+      .post(urlToken)
+      .send({ email: userData.email, password: userData.password });
+
+    const userToken = authResponse.body.token;
+    const url = "/movie_notes";
+
+    const response = await supertest(App)
+      .post(url)
+      .set("Authorization", `Bearer ${userToken}`)
+      .send(movieNote);
+
+    expect(response.status).toEqual(201);
+  });
+});
