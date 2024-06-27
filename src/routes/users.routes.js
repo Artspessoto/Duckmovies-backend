11e36378dacbb86fc6569587c98f84f19a796231
artspessoto <artspessoto@gmail.com>
2024-06-27 14:10:51 -0300

    update upload.js and create patch route to users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require('../controllers/UsersController');
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
@@ -6,8 +8,13 @@ const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 const usersController = new UsersController();
 
 const userRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 userRoutes.post("/", usersController.create);
 userRoutes.put('/', ensureAuthenticated, usersController.update);
+
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename)
+});
 
 module.exports = userRoutes;
