4c470d6158954ada3f79ca2612909594b0eb50ff
artspessoto <artspessoto@gmail.com>
2024-06-27 15:42:39 -0300

    Finish controller to save and load image upload

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -1,6 +1,33 @@
 const AppError = require("../utils/AppError");
-const knex = require('../database/knex');
+const knex = require("../database/knex");
+const DiskStorage = require("../providers/DiskStorage");
 
 class UserAvatarController {
-    
+  async update(req, res) {
+    const user_id = req.user.id;
+    const avatarFilename = req.file.filename;
+
+    const diskStorage = new DiskStorage();
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user)
+      throw new AppError(
+        "Somente usuários autenticados podem mudar o avatar",
+        401
+      );
+
+    if (user.avatar) {
+      await diskStorage.deleteFile(user.avatar);
+    }
+
+    const filename = await diskStorage.saveFile(avatarFilename);
+    user.avatar = filename;
+
+    await knex("users").update(user).where({ id: user_id });
+
+    return res.status(200).json(user);
+  }
 }
+
+module.exports = UserAvatarController;
