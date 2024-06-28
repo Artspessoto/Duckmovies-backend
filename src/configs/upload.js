7689284d58d7cb5068cb3216f4d17af63200c035
artspessoto <artspessoto@gmail.com>
2024-06-27 21:37:32 -0300

    add feature allowed extensions for upload

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,9 +1,18 @@
 const path = require("path");
 const multer = require("multer");
 const crypto = require("crypto");
+const AppError = require("../utils/AppError");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
+
+const ALLOWED_EXTENSIONS = [".jpeg", ".jpg", ".png", ".gif"];
+
+const fileFilter = (req, file, cb) => {
+  const extname = path.extname(file.originalname).toLowerCase();
+  if (ALLOWED_EXTENSIONS.includes(extname)) return cb(null, true);
+  cb(new AppError("Apenas imagens com extensão jpeg, jpg, png ou gif são permitidas.", 401));
+};
 
 const MULTER = {
   storage: multer.diskStorage({
@@ -15,6 +24,7 @@ const MULTER = {
       return cb(null, fileName);
     },
   }),
+  fileFilter,
 };
 
 module.exports = {
