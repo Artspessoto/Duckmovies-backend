497e6e9d998a27fa7d232912ee69f8f569fefca6
artspessoto <artspessoto@gmail.com>
2024-07-02 13:51:20 -0300

    add test for upload user avatar

---
+++ src/routes/__tests__/userAvatar.routes.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest"
+import supertest from "supertest"
+
+import { App } from "../../server.js";
+
+describe("User Avatar", () => {
+it("should upload avatar for an user", async() => {
+    
+})
+})
