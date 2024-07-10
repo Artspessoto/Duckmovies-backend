b04a78e41c7277a1477ca324875eae9685c0dbf5
artspessoto <artspessoto@gmail.com>
2024-07-10 17:34:05 -0300

    installing pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,13 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    name: "app",
+    script: "./app.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
     }
-  }
-};
+  }]
+}
