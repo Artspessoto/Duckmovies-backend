59c7ee2d44ea8730b870ae4dbf46b9a89e627418
artspessoto <artspessoto@gmail.com>
2024-07-18 13:14:15 -0300

    feature user and session response

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,5 @@
-const env = process.env.NODE_ENV === "test" ? ".env.example" : "prod.env";
+const env = process.env.NODE_ENV === "production" ? "prod.env" : ".env.example" ;
 require("dotenv").config({ path: env })
-
 module.exports = {
   jwt: {
     secret: process.env.JWT_SECRET,
